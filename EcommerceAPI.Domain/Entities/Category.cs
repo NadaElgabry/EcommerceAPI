e52ecd3cf1ee627d30126bf5d938cd3a2feeafdd
Mohamed ElGabry <mohammed.elgabry7@gmail.com>
2026-08-06 13:42:57 +0300

    Initial Commit for Entities Addition - Product and Category entities added

---
+++ EcommerceAPI.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace EcommerceAPI.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int? ParentId { get; set; }
+        public Category? ParentCategory { get; set; }
+
+        public List<Category> SubCategories { get; set; } = new List<Category>();
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ EcommerceAPI.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace EcommerceAPI.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Slug { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string Ingrediants { get; set; }
+        public string StockQuantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal? SalePrice { get; set; }
+        public decimal? DiscountPercentage { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
