7b1d9c84378e3429ce37512c3e87c5c5531071eb
Ahmed Loay <ahmed.ali041@eng-st.cu.edu.eg>
2026-08-10 11:44:59 +0300

    Add general ApiResponse

---
+++ EcommerceAPI/Common/ApiResponse.cs
@@ -0,0 +1,32 @@
+﻿namespace EcommerceAPI.Application.Common
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; } = true;
+        public int StatusCode { get; set; } = 200;
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+
+        public static ApiResponse<T> SuccessResponse(T data, string? message = null, int statusCode = 200)
+        {
+            return new ApiResponse<T>
+            {
+                Success = true,
+                StatusCode = statusCode,
+                Data = data,
+                Message = message
+            };
+        }
+
+        public static ApiResponse<T> SuccessResponse(string? message = null, int statusCode = 200)
+        {
+            return new ApiResponse<T>
+            {
+                Success = true,
+                StatusCode = statusCode,
+                Data = default,
+                Message = message
+            };
+        }
+    }
+}
--- EcommerceAPI/Controllers/AuthController.cs
+++ EcommerceAPI/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using EcommerceAPI.Application.DTOs.Auth;
 using Microsoft.AspNetCore.Mvc;
 using EcommerceAPI.Application.Interfaces.IServices;
+using EcommerceAPI.Application.Common;
 
 namespace EcommerceAPI.Controllers
 {
@@ -20,7 +21,7 @@ namespace EcommerceAPI.Controllers
         public async Task<IActionResult> Login([FromBody] LoginRequest request, CancellationToken cancellationToken)
         {
             var result = await _authService.Login(request, cancellationToken);
-            return Ok(result);
+            return Ok(ApiResponse<AuthResponse>.SuccessResponse(message: "Login successful", statusCode: 200, data: result));
         }
 
         [HttpPost("register")]
@@ -33,35 +34,35 @@ namespace EcommerceAPI.Controllers
                     cancellationToken
                 );
 
-            return Ok(token);
+            return Created("Register",ApiResponse<string>.SuccessResponse(message: "User created successfully", statusCode: 201, data: token));
         }
 
         [HttpPost("ActivateAccount")]
         public async Task<IActionResult> ActivateAccount([FromBody] ActivateEmailRequest request, CancellationToken cancellationToken)
         {
             var response = await _authService.ActivateEmailAsync(request, cancellationToken);
-            return Ok(response);
+            return Ok(ApiResponse<AuthResponse>.SuccessResponse(message: "Email activated successfully", statusCode: 200, data: response));
         }
 
         [HttpPost("IsEmailAvailable")]
         public async Task<IActionResult> IsEmailAvailable([FromBody] EmailRequest request, CancellationToken cancellationToken)
         {
             var isAvailable = await _authService.IsEmailAvailable(request, cancellationToken);
-            return Ok(isAvailable);
+            return Ok(ApiResponse<bool>.SuccessResponse(message: "Email availability checked", statusCode: 200, data: isAvailable));
         }
 
         [HttpPost("refresh")]
         public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequest request, CancellationToken cancellationToken)
         { 
             var response = await _authService.Refresh(request, cancellationToken);
-            return Ok(response);
+            return Ok(ApiResponse<AuthResponse>.SuccessResponse(message: "Token refreshed successfully", statusCode: 200, data: response));
         }
 
         [HttpPost("logout")]
         public async Task<IActionResult> Logout([FromBody] LogoutRequest request, CancellationToken cancellationToken) 
         { 
             await _authService.Logout(request, cancellationToken);
-            return NoContent();        
+            return StatusCode(204, ApiResponse<string>.SuccessResponse(message: "Logged out successfully", statusCode: 204));        
         }
     }
 }
