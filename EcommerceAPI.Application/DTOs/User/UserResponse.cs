b2dfffa8b99cf8eec93f2ce9f0f7e805f4b26fca
Mohamed ElGabry <mohammed.elgabry7@gmail.com>
2026-09-02 10:56:39 +0300

    Added to User Response Perfered Category

--- EcommerceAPI.Application/DTOs/User/UserResponse.cs
+++ EcommerceAPI.Application/DTOs/User/UserResponse.cs
@@ -1,7 +1,5 @@
 ﻿using EcommerceAPI.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using EcommerceAPI.Domain.Entities;
 
 namespace EcommerceAPI.Application.DTOs.User
 {
@@ -14,6 +12,7 @@ namespace EcommerceAPI.Application.DTOs.User
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
+        public List<FavoriteCategory> PreferredCategories { get; set; }
         public Role Role { get; set; }
         public bool IsActive {  get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
--- EcommerceAPI.Application/Mappers/Mappings/UserMapper.cs
+++ EcommerceAPI.Application/Mappers/Mappings/UserMapper.cs
@@ -16,6 +16,7 @@ namespace EcommerceAPI.Application.Mappers.Mappings
                 FirstName = user.FirstName,
                 LastName = user.LastName,
                 Email = user.Email,
+                PreferredCategories = user.PreferredCategories,
                 BirthDate = user.BirthDate,
                 Role = user.Role,
                 PhoneNumber = user.PhoneNumber,
