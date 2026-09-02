10a10b81e02ef10cb11b9e638b01270d60e4ff38
Mohamed ElGabry <mohammed.elgabry7@gmail.com>
2026-09-02 10:57:13 +0300

    added nullable to the preffered categories part

--- EcommerceAPI.Application/DTOs/User/UserResponse.cs
+++ EcommerceAPI.Application/DTOs/User/UserResponse.cs
@@ -12,7 +12,7 @@ namespace EcommerceAPI.Application.DTOs.User
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
-        public List<FavoriteCategory> PreferredCategories { get; set; }
+        public List<FavoriteCategory>? PreferredCategories { get; set; }
         public Role Role { get; set; }
         public bool IsActive {  get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
