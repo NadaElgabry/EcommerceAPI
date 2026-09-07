f3c129d9d7f3304338a9071653819f36a175c4fd
Mohamed ElGabry <mohammed.elgabry7@gmail.com>
2026-09-07 15:25:33 +0300

    added fluent validator

---
+++ EcommerceAPI.Application/Validators/Order/GetAllOrdersRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using EcommerceAPI.Domain.Enums;
+using FluentValidation;
+
+namespace EcommerceAPI.Application.Validators.Order
+{
+    public class GetAllOrdersRequestValidator : AbstractValidator<GetAllOrdersRequest>
+    {
+        public GetAllOrdersRequestValidator()
+        {
+            RuleFor(request => request.Status)
+                .Must(status => string.IsNullOrEmpty(status) || Enum.TryParse<OrderStatus>(status, ignoreCase: true, out _))
+                .WithMessage($"Status must be one of the following: {string.Join(", ", Enum.GetNames<OrderStatus>())}.");
+        }
+    }
+}
